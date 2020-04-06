b90c1222491edb618594c8dcdeb57c679dd4b91a
9919 <37637043+MozJer@users.noreply.github.com>
2020-04-07 00:17:35 +0300

    apis creation

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.views import Response
+from rest_framework import status
 
-# Create your views here.
+from profiles_api import serializers
+
+
+class HelloApiView(APIView):
+    """a class to test it"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'ha 1',
+            'he 2',
+            'ho 3',
+            'h 4'
+        ]
+        return Response({'message': 'HelloMan!', 'an_api': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello bro {name}'
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
+
+    def cdshgosldn(self, request, pk=None):
+        return Response({'method': 'DELETE'})
